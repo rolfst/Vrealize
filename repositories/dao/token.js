d3eec981ee3a6f57fc60b13eb301e184bffbced6
Rolf Strijdhorst <rolfst@gmail.com>
2016-03-29 13:17:57 +0200

    changed tenant to username

--- repositories/dao/token.js
+++ repositories/dao/token.js
@@ -3,7 +3,7 @@ var mongoose = require('../mongoose');
 var mongooseSchema = mongoose.Schema;
 
 var schema = mongooseSchema({
-  tenant: { type: String, required: true, index: { unique: true }}, // identifier to get token
+  username: { type: String, required: true, index: { unique: true }}, // identifier to get token
   expiry: { type: String, required: true }, // expiry time from token
   token: { type: String, required: true, default: null }, // token
   createdAt: { type: Date, default: Date.now, expires: '8h' }
