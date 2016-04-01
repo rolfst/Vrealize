c91d14643dc246402bfcfe89c6d46ba5f7aea2cb
Francisco Temudo <fthemudo@gmail.com>
2016-04-01 16:07:04 +0100

    Adds remaining tests for get

--- test/integration/services/vpc_test.js
+++ test/integration/services/vpc_test.js
@@ -7,6 +7,7 @@ var config = require('../../../config');
 var dbUri = config.get('database').uri;
 var vpcConfig = config.get('vpcConfig');
 var logger = require('../../../logger');
+var stubs = require('../../stubs');
 logger = logger.getLogger('vpc integration');
 
 var clearDB = require('mocha-mongoose');
@@ -24,10 +25,6 @@ describe('VPC Service Integration', function () {
   describe('list', function () {
     var tokenValue = '#$#$#cdd^3@!fe9203&8Az==';
     var credentials;
-    afterEach(function (done) {
-      clearDB(dbUri);
-      done();
-    });
     beforeEach(function (done) {
       credentials = {
         username: 'storedToken',
@@ -69,6 +66,7 @@ describe('VPC Service Integration', function () {
 
   describe('#get', function () {
     var params;
+    var tokenValue = '#$#$#cdd^3@!fe9203&8Az==';
 
     beforeEach(function () {
       params = {
@@ -84,6 +82,43 @@ describe('VPC Service Integration', function () {
         delete params[param];
         target.get(params, null, function (err) {
           err.code.should.equal(400);
+          done();
+        });
+      });
+    });
+
+    describe('when params are ok', function () {
+      var credentials;
+      beforeEach(function (done) {
+        params = {
+          tenant: 'someTenant',
+          username: 'someUser',
+          password: 'somePass',
+          resourceId: 'someResourceId'
+        };
+        credentials = {
+          tenant: 'someTenant',
+          username: 'someUser',
+          password: 'somePass'
+        };
+        var token = _.merge({},
+          {
+            token: tokenValue,
+            expiry: moment().subtract(1, 'hours').toDate()
+          }, credentials);
+        repoHelper.createOne(token).then(function () {
+          done();
+        });
+      });
+
+      it('should return a vm', function (done) {
+        var request = nock(vpcConfig.baseUrl)
+        .get(resourcesPath + 'someResourceId')
+        .reply(200, stubs.windows_vm);
+        target.get(params, null, function callback(error, value) {
+          request.done();
+          should.not.exist(error);
+          value.should.eql(stubs.windows_vm);
           done();
         });
       });
