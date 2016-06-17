b648bf91608cc49f0b7dc64010e01697bf7ef5b8
Nick <nick.van.eijk@itility.nl>
2016-06-17 13:51:46 +0200

    Added tests to verify new output

--- test/unit/repositories/mapper_test.js
+++ test/unit/repositories/mapper_test.js
@@ -5,7 +5,7 @@ var blueprints = require('../../blueprints/index');
 
 var target = require('../../../repositories/mapper');
 
-describe.only('Mapper', function () {
+describe('Mapper', function () {
   describe('toComputeInstance', function () {
     it('should translate a VPC machine to a compute instance', function () {
       var expected = blueprints.compute_instance_vpc_windows;
